73215b9449d4d403cc2448d84bc7db763d59ddfe
Postmodern <postmodern.mod3@gmail.com>
2021-12-08 15:51:24 -0800

    Changed the project name to ronin-payloads.

--- lib/ronin/payloads/helpers/shell.rb
+++ lib/ronin/payloads/helpers/shell.rb
@@ -1,23 +1,23 @@
 #
-# ronin-exploits - A Ruby library for ronin-rb that provides exploitation and
-# payload crafting functionality.
+# ronin-payloads - A Ruby micro-framework for writing and running exploit
+# payloads.
 #
 # Copyright (c) 2007-2013 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
-# This file is part of ronin-exploits.
+# This file is part of ronin-payloads.
 #
-# ronin-exploits is free software: you can redistribute it and/or modify
+# ronin-payloads is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ronin-exploits is distributed in the hope that it will be useful,
+# ronin-payloads is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with ronin-exploits.  If not, see <https://www.gnu.org/licenses/>
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>
 #
 
 module Ronin
