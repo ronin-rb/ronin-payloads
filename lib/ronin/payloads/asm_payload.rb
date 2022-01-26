df4c6607ee1c34104d0c7e6313cd3276b55078d8
Postmodern <postmodern.mod3@gmail.com>
2022-01-26 05:57:26 -0800

    Changed the license from GPL-3.0 to LGPL-3.0.

--- lib/ronin/payloads/asm_payload.rb
+++ lib/ronin/payloads/asm_payload.rb
@@ -7,17 +7,17 @@
 # This file is part of ronin-payloads.
 #
 # ronin-payloads is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # ronin-payloads is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 require 'ronin/payloads/binary_payload'
