8269f73b19a76cdff0658d913e850c60a75da363
Postmodern <postmodern.mod3@gmail.com>
2022-01-26 06:19:17 -0800

    Added a `ronin/payloads/version` file.

---
+++ lib/ronin/payloads/version.rb
@@ -0,0 +1,28 @@
+#
+# ronin-payloads - A Ruby micro-framework for writing and running exploit
+# payloads.
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-payloads.
+#
+# ronin-payloads is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-payloads is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+module Ronin
+  module Payloads
+    # ronin-payloads version
+    VERSION = '0.1.0'
+  end
+end
