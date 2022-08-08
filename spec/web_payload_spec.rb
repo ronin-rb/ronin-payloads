0bd387bc273a2fa0d26f46cc17c7ce948e471ddf
Postmodern <postmodern.mod3@gmail.com>
2022-08-07 19:20:52 -0700

    Removed `Ronin::Payloads::WebPayload`.

--- spec/web_payload_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require 'ronin/payloads/web_payload'
-
-describe Ronin::Payloads::WebPayload do
-  it "must inherit from Ronin::Payloads::Payload" do
-    expect(described_class.superclass).to be(Ronin::Payloads::Payload)
-  end
-end
