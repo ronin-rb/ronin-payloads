a1b670cea01bea69eea554d7998504ca3d1e9457
Postmodern <postmodern.mod3@gmail.com>
2022-09-14 14:38:53 -0700

    Added specs for `Ronin::Payloads`.

---
+++ spec/payloads_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'ronin/payloads'
+
+describe Ronin::Payloads do
+  it "should have a version" do
+    expect(subject.const_defined?('VERSION')).to eq(true)
+  end
+
+  it "must include Ronin::Core::ClassRegistry" do
+    expect(subject).to include(Ronin::Core::ClassRegistry)
+  end
+
+  it "must include Ronin::Repos::ClassDir" do
+    expect(subject).to include(Ronin::Repos::ClassDir)
+  end
+
+  it "must set .class_dir to 'ronin/payloads/builtin'" do
+    expect(subject.class_dir).to eq('ronin/payloads/builtin')
+  end
+
+  it "must set .repo_class_dir to 'payloads'" do
+    expect(subject.repo_class_dir).to eq('payloads')
+  end
+end
