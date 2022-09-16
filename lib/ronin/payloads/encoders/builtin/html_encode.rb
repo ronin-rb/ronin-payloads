d19cdf6dfa260da03b73bb3afaed4c3e26331cb0
Postmodern <postmodern.mod3@gmail.com>
2022-09-15 22:38:31 -0700

    Added `Ronin::Payloads::Encoders::HTMLEncode`.

---
+++ lib/ronin/payloads/encoders/builtin/html_encode.rb
@@ -0,0 +1,54 @@
+#
+# ronin-payloads - A Ruby micro-framework for writing and running exploit
+# payloads.
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-payloads is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-payloads is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/payloads/encoders/html_encoder'
+require 'ronin/support/encoding/html'
+
+module Ronin
+  module Payloads
+    module Encoders
+      class HTMLEncode < HTMLEncoder
+
+        register 'html_encode'
+
+        summary 'Encodes every character as a HTML special character'
+
+        description <<~DESC
+          Encodes every character in the given String as an HTML special character:
+
+            hello world -> &#104;&#101;&#108;&#108;&#111;&#32;&#119;&#111;&#114;&#108;&#100;
+
+        DESC
+
+        #
+        # HTML encodes the given data.
+        #
+        # @param [String] data
+        #
+        # @return [String]
+        #
+        def encode(data)
+          Support::Encoding::HTML.encode(data)
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/encoders/builtin/html_encode_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'ronin/payloads/encoders/builtin/html_encode'
+
+describe Ronin::Payloads::Encoders::HTMLEncode do
+  it "must inherit from Ronin::Payloads::Encoders::HTMLEncoder" do
+    expect(described_class).to be < Ronin::Payloads::Encoders::HTMLEncoder
+  end
+
+  describe "#encode" do
+    let(:data)    { "hello world" }
+    let(:encoded) { "&#104;&#101;&#108;&#108;&#111;&#32;&#119;&#111;&#114;&#108;&#100;" }
+
+    it "must HTML encode each character" do
+      expect(subject.encode(data)).to eq(encoded)
+    end
+  end
+end
