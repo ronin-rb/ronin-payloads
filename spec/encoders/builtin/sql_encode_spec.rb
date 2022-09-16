d4dc23ea8a60c15c5b800a1386fc28513f96f517
Postmodern <postmodern.mod3@gmail.com>
2022-09-15 22:41:59 -0700

    Added `Encoders::SQLEncode`.

---
+++ spec/encoders/builtin/sql_encode_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'ronin/payloads/encoders/builtin/sql_encode'
+
+describe Ronin::Payloads::Encoders::SQLEncode do
+  it "must inherit from Ronin::Payloads::Encoders::SQLEncoder" do
+    expect(described_class).to be < Ronin::Payloads::Encoders::SQLEncoder
+  end
+
+  describe "#encode" do
+    let(:data)    { "hello world" }
+    let(:encoded) { "0x68656c6c6f20776f726c64" }
+
+    it "must SQL encode the given String" do
+      expect(subject.encode(data)).to eq(encoded)
+    end
+  end
+end
