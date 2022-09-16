51bf8b72abe2c5c70c70c6e5c5ddefd2b2f66fe3
Postmodern <postmodern.mod3@gmail.com>
2022-09-15 23:20:11 -0700

    Forgot to add `ronin/payloads/encoders/builtin/sql/encode`.

---
+++ lib/ronin/payloads/encoders/builtin/sql/encode.rb
@@ -0,0 +1,56 @@
+#
+# ronin-payloads - A Ruby micro-framework for writing and running exploit
+# payloads.
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-payloads is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-payloads is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/payloads/encoders/sql_encoder'
+require 'ronin/support/encoding/sql'
+
+module Ronin
+  module Payloads
+    module Encoders
+      module SQL
+        class Encode < SQLEncoder
+
+          register 'sql/encode'
+
+          summary 'Encodes the data as a SQL hex string'
+
+          description <<~DESC
+            Encodes the given String as a SQL hex string:
+
+              hello world -> 0x68656c6c6f20776f726c64
+
+          DESC
+
+          #
+          # SQL encodes the given data.
+          #
+          # @param [String] data
+          #
+          # @return [String]
+          #
+          def encode(data)
+            Support::Encoding::SQL.encode(data)
+          end
+
+        end
+      end
+    end
+  end
+end
