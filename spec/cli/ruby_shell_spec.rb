38a7121b151497303006b1396bd4eb56c686bc98
Postmodern <postmodern.mod3@gmail.com>
2022-09-21 17:37:59 -0700

    Added specs for `CLI::RubyShell`.

---
+++ spec/cli/ruby_shell_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'ronin/payloads/cli/ruby_shell'
+
+describe Ronin::Payloads::CLI::RubyShell do
+  describe "#initialize" do
+    it "must default #name to 'ronin-payloads'" do
+      expect(subject.name).to eq('ronin-payloads')
+    end
+
+    it "must default #context to Ronin::Payloads" do
+      expect(subject.context).to be(Ronin::Payloads)
+    end
+  end
+end
