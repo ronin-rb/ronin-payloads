7db671cff37e3c1a41fd2b30f24f54abf6ae286b
Postmodern <postmodern.mod3@gmail.com>
2022-12-05 17:47:19 -0800

    Added `Ronin::Payloads::Builtin::Test::XSS`.

---
+++ spec/builtin/test/xss_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'ronin/payloads/builtin/test/xss'
+
+describe Ronin::Payloads::Test::XSS do
+  it "must inherit from Ronin::Payloads::JavaScriptPayload" do
+    expect(described_class).to be < Ronin::Payloads::JavaScriptPayload
+  end
+
+  describe "#build" do
+    before { subject.build }
+
+    it "must set #payload to 'alert(1)'" do
+      expect(subject.payload).to eq("alert(1)")
+    end
+  end
+end
