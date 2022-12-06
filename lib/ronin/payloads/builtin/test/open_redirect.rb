cd7ce6c51eaa5c2c6dfd3310c8ffca3726ce8045
Postmodern <postmodern.mod3@gmail.com>
2022-12-05 18:28:15 -0800

    Added `Ronin::Payloads::Test::OpenRedirect`.

---
+++ lib/ronin/payloads/builtin/test/open_redirect.rb
@@ -0,0 +1,49 @@
+#
+# ronin-payloads - A Ruby micro-framework for writing and running exploit
+# payloads.
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-payloads is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-payloads is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/payloads/url_payload'
+
+module Ronin
+  module Payloads
+    module Test
+      #
+      # A test payload for Open Redirects.
+      #
+      class OpenRedirect < URLPayload
+
+        register 'test/open_redirect'
+
+        summary 'An Open Redirect test payload'
+        description <<~DESC
+        A non-malicious test payload for testing Open Redirect vulnerabilities.
+        Simply redirects to https://google.com/.
+        DESC
+        
+        #
+        # Builds the Open Redirect test payload.
+        #
+        def build
+          @payload = 'https://google.com/'
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/builtin/test/open_redirect_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'ronin/payloads/builtin/test/open_redirect'
+
+describe Ronin::Payloads::Test::OpenRedirect do
+  it "must inherit from Ronin::Payloads::URLPayload" do
+    expect(described_class).to be < Ronin::Payloads::URLPayload
+  end
+
+  describe "#build" do
+    before { subject.build }
+
+    it "must set #payload to 'https://google.com/'" do
+      expect(subject.payload).to eq("https://google.com/")
+    end
+  end
+end
