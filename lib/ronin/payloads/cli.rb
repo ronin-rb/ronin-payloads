f2dc60ac9fb45808aa7af9892c17b32f071b06f0
Postmodern <postmodern.mod3@gmail.com>
2023-01-02 17:35:57 -0800

    Ensure that `ronin-payloads --version` prints the version.

--- lib/ronin/payloads/cli.rb
+++ lib/ronin/payloads/cli.rb
@@ -19,8 +19,11 @@
 # along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+require 'ronin/payloads/version'
+
 require 'command_kit/commands'
 require 'command_kit/commands/auto_load'
+require 'command_kit/options/version'
 
 module Ronin
   module Payloads
@@ -36,8 +39,10 @@ module Ronin
         dir:       "#{__dir__}/cli/commands",
         namespace: "#{self}::Commands"
       )
+      include CommandKit::Options::Version
 
       command_name 'ronin-payloads'
+      version Ronin::Payloads::VERSION
 
       command_aliases['ls']   = 'list'
       command_aliases['info'] = 'show'
