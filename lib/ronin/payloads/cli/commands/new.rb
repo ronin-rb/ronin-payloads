00994068ce70b08ec84010051a275a1486f21530
Postmodern <postmodern.mod3@gmail.com>
2023-01-16 14:02:30 -0800

    Fixed license headers.

--- lib/ronin/payloads/cli/commands/new.rb
+++ lib/ronin/payloads/cli/commands/new.rb
@@ -2,18 +2,18 @@
 #
 # Copyright (c) 2021 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
-# ronin-repos is free software: you can redistribute it and/or modify
+# ronin-payloads is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ronin-repos is distributed in the hope that it will be useful,
+# ronin-payloads is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with ronin-repos.  If not, see <https://www.gnu.org/licenses/>.
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 require 'ronin/payloads/cli/command'
