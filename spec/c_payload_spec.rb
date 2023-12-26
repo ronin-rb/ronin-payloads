846fd7fd8f6c0c913679290e9f5c58d6ba087627
Postmodern <postmodern.mod3@gmail.com>
2023-12-26 06:47:05 -0800

    Renamed `Mixins::CC` to `Mixins::CCompiler`.

--- spec/c_payload_spec.rb
+++ spec/c_payload_spec.rb
@@ -6,7 +6,7 @@ describe Ronin::Payloads::CPayload do
     expect(described_class.superclass).to be(Ronin::Payloads::BinaryPayload)
   end
 
-  it "must include Ronin::Payloads::Mixins::CC" do
-    expect(described_class).to include(Ronin::Payloads::Mixins::CC)
+  it "must include Ronin::Payloads::Mixins::CCompiler" do
+    expect(described_class).to include(Ronin::Payloads::Mixins::CCompiler)
   end
 end
