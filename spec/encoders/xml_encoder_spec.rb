ee490fcc0fc3bd43b528caa271e26aece95a8339
Postmodern <postmodern.mod3@gmail.com>
2024-08-06 11:55:24 -0700

    Added `.encoder_type` methods to all `Encoder` classes (closes #149).

--- spec/encoders/xml_encoder_spec.rb
+++ spec/encoders/xml_encoder_spec.rb
@@ -5,4 +5,12 @@ describe Ronin::Payloads::Encoders::XMLEncoder do
   it "must inherit from Ronin::Payloads::Encoders::Encoder" do
     expect(described_class).to be < Ronin::Payloads::Encoders::Encoder
   end
+
+  describe ".encoder_type" do
+    subject { described_class }
+
+    it "must return :xml" do
+      expect(subject.encoder_type).to eq(:xml)
+    end
+  end
 end
