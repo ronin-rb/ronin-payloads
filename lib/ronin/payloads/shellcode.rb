c3cf2a2bb88138bfba786f8eba7404cfda125ecb
Postmodern <postmodern.mod3@gmail.com>
2024-08-09 00:40:26 -0700

    Added the missing `ronin/payloads/shellcode` file.

---
+++ lib/ronin/payloads/shellcode.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+#
+# ronin-payloads - A Ruby micro-framework for writing and running exploit
+# payloads.
+#
+# Copyright (c) 2007-2024 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-payloads is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-payloads is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require_relative 'shellcode/bind_shell_payload'
+require_relative 'shellcode/exec_shell_payload'
+require_relative 'shellcode/reverse_shell_payload'
