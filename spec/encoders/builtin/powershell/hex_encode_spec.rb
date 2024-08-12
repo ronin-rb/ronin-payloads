c49b3c54ddfb8dd2132bfc00c636520696ec11d8
Postmodern <postmodern.mod3@gmail.com>
2024-08-12 13:58:15 -0700

    Replace `powershell/encode` encoder with `powershell/hex_encode` (closes #152).

---
+++ spec/encoders/builtin/powershell/hex_encode_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'ronin/payloads/encoders/builtin/powershell/hex_encode'
+
+describe Ronin::Payloads::Encoders::PowerShell::HexEncode do
+  it "must inherit from Ronin::Payloads::Encoders::PowerShellEncoder" do
+    expect(described_class).to be < Ronin::Payloads::Encoders::PowerShellEncoder
+  end
+
+  describe "#encode" do
+    let(:data)    { "dir" }
+    let(:encoded) { "Invoke-Expression \"$([char]0x64)$([char]0x69)$([char]0x72)\"" }
+
+    it "must each character of the command as PowerShell '$([char]0xXX)' characters and evaluate the resulting string using 'Invoke-Expression'" do
+      expect(subject.encode(data)).to eq(encoded)
+    end
+  end
+end
