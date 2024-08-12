2845a2706da135052815e44f62ae57b13e8c3b43
Postmodern <postmodern.mod3@gmail.com>
2024-08-12 14:34:28 -0700

    Replaced the `js/encode` encoder with `js/hex_encode` (closes #154).

---
+++ spec/encoders/builtin/js/hex_encode_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'ronin/payloads/encoders/builtin/js/hex_encode'
+
+describe Ronin::Payloads::Encoders::JS::HexEncode do
+  it "must inherit from Ronin::Payloads::Encoders::JavaScriptEncoder" do
+    expect(described_class).to be < Ronin::Payloads::Encoders::JavaScriptEncoder
+  end
+
+  describe "#encode" do
+    let(:command) { "alert(1)" }
+    let(:encoded) { "eval(\"\\x61\\x6C\\x65\\x72\\x74\\x28\\x31\\x29\")" }
+
+    it "must encode each character of the given String as a hex escaped character and evaluate the resulting String using 'eval()'" do
+      expect(subject.encode(command)).to eq(encoded)
+    end
+  end
+end
