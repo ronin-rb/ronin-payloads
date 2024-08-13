9710cefef5e48db437b971cfcde0c536bec10da7
Postmodern <postmodern.mod3@gmail.com>
2024-08-12 19:43:50 -0700

    Added `JavaScriptPayload#to_html` (closes #138).

--- spec/javascript_payload_spec.rb
+++ spec/javascript_payload_spec.rb
@@ -9,4 +9,23 @@ describe Ronin::Payloads::JavaScriptPayload do
       expect(subject.encoder_class).to be(Ronin::Payloads::Encoders::JavaScriptEncoder)
     end
   end
+
+  describe "#to_html" do
+    class TestJavaScriptPayloadToHtml < Ronin::Payloads::JavaScriptPayload
+
+      def build
+        @payload = "alert(1)"
+      end
+
+    end
+
+    let(:payload_class) { TestJavaScriptPayloadToHtml }
+    subject { payload_class.new }
+
+    it "must wrap the built payload in '<script type=\"text/javascript\">...</script>'" do
+      expect(subject.to_html).to eq(
+        %{<script type="text/javascript">#{subject}</script>}
+      )
+    end
+  end
 end
