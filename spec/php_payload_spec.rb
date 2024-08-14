a7df8fa5f0bbf854011132383bd8d42af17dba22
Postmodern <postmodern.mod3@gmail.com>
2024-08-14 01:11:13 -0700

    Added `PHPPayload#to_command` (closes #134).

--- spec/php_payload_spec.rb
+++ spec/php_payload_spec.rb
@@ -5,4 +5,23 @@ describe Ronin::Payloads::PHPPayload do
   it "must inherit from Ronin::Payloads::Payload" do
     expect(described_class.superclass).to be(Ronin::Payloads::Payload)
   end
+
+  module TestPHPPayload
+    class TestPayload < Ronin::Payloads::PHPPayload
+
+      def build
+        @payload = "echo 'PWNED';"
+      end
+
+    end
+  end
+
+  let(:payload_class) { TestPHPPayload::TestPayload }
+  subject { payload_class.new }
+
+  describe "#to_command" do
+    it "must escape the built payload as a quoted shell string and embed it within a 'php -r \"...\"' command" do
+      expect(subject.to_command).to eq(%{php -r "echo 'PWNED';"})
+    end
+  end
 end
