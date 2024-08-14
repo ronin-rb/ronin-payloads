42bfe0d4afa7fd34a6c1ab19c514f69ae768e8d6
Postmodern <postmodern.mod3@gmail.com>
2024-08-13 22:16:29 -0700

    Added `CommandPayload#to_ruby` (closes #144).

--- spec/command_payload_spec.rb
+++ spec/command_payload_spec.rb
@@ -17,4 +17,23 @@ describe Ronin::Payloads::CommandPayload do
       expect(subject.encoder_class).to be(Ronin::Payloads::Encoders::CommandEncoder)
     end
   end
+
+  module TestCommandPayload
+    class TestPayload < Ronin::Payloads::CommandPayload
+
+      def build
+        @payload = "ls -la"
+      end
+
+    end
+  end
+
+  let(:payload_class) { TestCommandPayload::TestPayload }
+  subject { payload_class.new }
+
+  describe "#to_ruby" do
+    it "must convert the built command into a Ruby string that is passed to 'system(...)'" do
+      expect(subject.to_ruby).to eq("system(\"#{subject}\")")
+    end
+  end
 end
