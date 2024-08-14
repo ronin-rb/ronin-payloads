d114812337eb38d50b608ead3e7365fa8d290ca3
Postmodern <postmodern.mod3@gmail.com>
2024-08-14 14:20:56 -0700

    Added the `ruby/base64_encode` encoder (closes #172).

---
+++ lib/ronin/payloads/encoders/builtin/ruby/base64_encode.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+#
+# ronin-payloads - A Ruby micro-framework for writing and running exploit
+# payloads.
+#
+# Copyright (c) 2007-2024 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-payloads is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-payloads is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-payloads.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/payloads/encoders/ruby_encoder'
+require 'ronin/support/encoding/base64'
+
+module Ronin
+  module Payloads
+    module Encoders
+      module Ruby
+        #
+        # A Ruby encoder that encodes the given Ruby code as a Base64 string,
+        # then decodes it using `Base64.decode64()`, and then evaluates the
+        # decoded Ruby code using `eval()`.
+        #
+        #    puts('PWNED') -> require 'base64'; eval(Base64.decode64("cHV0cygnUFdORUQnKQ=="))
+        #
+        # @since 0.3.0
+        #
+        class Base64Encode < RubyEncoder
+
+          register 'ruby/base64_encode'
+
+          summary 'Encodes Ruby as Base64'
+
+          description <<~DESC
+            Encodes the given Ruby code as a Base64 string, then decodes it
+            using `Base64.decode64()`, and then evaluates the decoded Ruby
+            code using `eval()`.
+
+              puts('PWNED') -> require 'base64'; eval(Base64.decode64("cHV0cygnUFdORUQnKQ=="))
+
+          DESC
+
+          #
+          # Encodes Ruby code as Base64.
+          #
+          # @param [String] ruby
+          #   The Ruby code to encode.
+          #
+          # @return [String]
+          #
+          def encode(ruby)
+            base64 = Support::Encoding::Base64.encode(ruby, mode: :strict)
+
+            %{require 'base64'; eval(Base64.decode64("#{base64}"))}
+          end
+
+        end
+      end
+    end
+  end
+end
