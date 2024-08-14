f83c996f344377d859245a2e7de920324725e26b
Postmodern <postmodern.mod3@gmail.com>
2024-08-13 17:03:05 -0700

    Renamed `ShellPayload` to `ShellCommandPayload` (closes #164).

--- spec/shell_payload_spec.rb
+++ spec/shell_payload_spec.rb
@@ -2,15 +2,7 @@ require 'spec_helper'
 require 'ronin/payloads/shell_payload'
 
 describe Ronin::Payloads::ShellPayload do
-  it "must inherit from Ronin::Payloads::CommandPayload" do
-    expect(described_class.superclass).to be(Ronin::Payloads::CommandPayload)
-  end
-
-  describe ".encoder_class" do
-    subject { described_class }
-
-    it do
-      expect(subject.encoder_class).to be(Ronin::Payloads::Encoders::ShellEncoder)
-    end
+  it "must be an alias for Ronin::Payloads::ShellCommandPayload" do
+    expect(described_class).to be(Ronin::Payloads::ShellCommandPayload)
   end
 end
