964bd454b29f197c26726e6f40f88789d0a36988
Postmodern <postmodern.mod3@gmail.com>
2024-08-14 15:00:41 -0700

    Added the `php/base64_encode` encoder (closes #174).

---
+++ spec/encoders/builtin/php/base64_encode_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'ronin/payloads/encoders/builtin/php/base64_encode'
+
+describe Ronin::Payloads::Encoders::PHP::Base64Encode do
+  it "must inherit from Ronin::Payloads::Encoders::PHPEncoder" do
+    expect(described_class).to be < Ronin::Payloads::Encoders::PHPEncoder
+  end
+
+  describe "#encode" do
+    let(:php)     { "echo 'PWNED';" }
+    let(:encoded) { %{eval(base64_decode("ZWNobyAnUFdORUQnOw=="));} }
+
+    it "must encode the given PHP code as a Base64 string and embed it into the 'eval(base64_decode(\"...\"));' string" do
+      expect(subject.encode(php)).to eq(encoded)
+    end
+  end
+end
