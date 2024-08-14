6c507bb2cd421321d51eb1437d16cb545e1d26af
Postmodern <postmodern.mod3@gmail.com>
2024-08-14 03:14:15 -0700

    Added `PowerShellPayload#to_command` (closes #179).

--- spec/powershell_payload_spec.rb
+++ spec/powershell_payload_spec.rb
@@ -13,4 +13,23 @@ describe Ronin::Payloads::PowerShellPayload do
       expect(subject.encoder_class).to be(Ronin::Payloads::Encoders::PowerShellEncoder)
     end
   end
+
+  module TestPowerShellPayload
+    class TestPayload < Ronin::Payloads::PowerShellPayload
+
+      def build
+        @payload = "Write-Output 'PWNED'"
+      end
+
+    end
+  end
+
+  let(:payload_class) { TestPowerShellPayload::TestPayload }
+  subject { payload_class.new }
+
+  describe "#to_command" do
+    it "must escape the built payload as a quoted shell string and embed it within a 'pwsh -Command \"...\"' command" do
+      expect(subject.to_command).to eq(%{pwsh -Command "Write-Output 'PWNED'"})
+    end
+  end
 end
