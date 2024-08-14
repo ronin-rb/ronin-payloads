2b1091377e4269cf088852054c95988ffbfa6a46
Postmodern <postmodern.mod3@gmail.com>
2024-08-14 01:04:35 -0700

    Added `PythonPayload#to_command` (closes #136).

--- spec/python_payload_spec.rb
+++ spec/python_payload_spec.rb
@@ -5,4 +5,23 @@ describe Ronin::Payloads::PythonPayload do
   it "must inherit from Ronin::Payloads::Payload" do
     expect(described_class.superclass).to be(Ronin::Payloads::Payload)
   end
+
+  module TestPythonPayload
+    class TestPayload < Ronin::Payloads::PythonPayload
+
+      def build
+        @payload = "print('PWNED')"
+      end
+
+    end
+  end
+
+  let(:payload_class) { TestPythonPayload::TestPayload }
+  subject { payload_class.new }
+
+  describe "#to_command" do
+    it "must escape the built payload as a quoted shell string and embed it within a 'python -c \"...\"' command" do
+      expect(subject.to_command).to eq(%{python -c "print('PWNED')"})
+    end
+  end
 end
