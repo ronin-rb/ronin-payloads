63ec9454bc73524851a9fbcfc5e65a816693c983
Postmodern <postmodern.mod3@gmail.com>
2024-08-14 01:01:26 -0700

    Added `NodeJSPayload#to_command` (closes #137).

--- spec/node_js_payload_spec.rb
+++ spec/node_js_payload_spec.rb
@@ -13,4 +13,23 @@ describe Ronin::Payloads::NodeJSPayload do
       expect(subject.encoder_class).to be(Ronin::Payloads::Encoders::NodeJSEncoder)
     end
   end
+
+  module TestNodeJSPayload
+    class TestPayload < Ronin::Payloads::NodeJSPayload
+
+      def build
+        @payload = "console.log('PWNED')"
+      end
+
+    end
+  end
+
+  let(:payload_class) { TestNodeJSPayload::TestPayload }
+  subject { payload_class.new }
+
+  describe "#to_command" do
+    it "must escape the built payload as a quoted shell string and embed it within a 'node -e \"...\"' command" do
+      expect(subject.to_command).to eq(%{node -e "console.log('PWNED')"})
+    end
+  end
 end
