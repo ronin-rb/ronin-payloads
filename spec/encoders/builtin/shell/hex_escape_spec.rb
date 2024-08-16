bd8c39f9554bb8ed5b63bd24e098c63f478ae26e
Postmodern <postmodern.mod3@gmail.com>
2024-08-16 01:00:47 -0700

    Wrap the hex escaped encoder output in `bash -c "..."`.
    * This is because on Ubuntu/Debian systems `sh` is actually `dash`,
      which does not support `$'\xXX...'` style strings. So we must ensure
      the hex escaped command is executed under `bash`.

--- spec/encoders/builtin/shell/hex_escape_spec.rb
+++ spec/encoders/builtin/shell/hex_escape_spec.rb
@@ -16,14 +16,14 @@ describe Ronin::Payloads::Encoders::Shell::HexEscape do
 
   describe "#encode" do
     let(:command) { "echo PWNED" }
-    let(:encoded) { "$'\\x65\\x63\\x68\\x6f' $'\\x50\\x57\\x4e\\x45\\x44'" }
+    let(:encoded) { %{bash -c "$'\\x65\\x63\\x68\\x6f' $'\\x50\\x57\\x4e\\x45\\x44'"} }
 
     it "must encode each argument in the given command string into a hex strings" do
       expect(subject.encode(command)).to eq(encoded)
     end
 
     it "must return a valid shell command", :integration do
-      expect(`bash -c "#{subject.encode(command)}"`).to eq("PWNED#{$/}")
+      expect(`#{subject.encode(command)}`).to eq("PWNED#{$/}")
     end
   end
 end
