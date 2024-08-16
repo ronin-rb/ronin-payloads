bc41f5d72c5419448cba0161a07ed8c9702faa9f
Postmodern <postmodern.mod3@gmail.com>
2024-08-15 19:20:43 -0700

    Use `exec()` to evaluate the Python code instead of `eval()` for Python 2.
    * Under Python2 `eval()` does not support `print` statements, but
      `exec()` does.

--- spec/encoders/builtin/python/hex_encode_spec.rb
+++ spec/encoders/builtin/python/hex_encode_spec.rb
@@ -16,9 +16,9 @@ describe Ronin::Payloads::Encoders::Python::HexEncode do
 
   describe "#encode" do
     let(:python)  { "print('PWNED')" }
-    let(:encoded) { %{import binascii; eval(binascii.unhexlify("7072696e74282750574e45442729"))} }
+    let(:encoded) { %{import binascii; exec(binascii.unhexlify("7072696e74282750574e45442729"))} }
 
-    it "must encode the given Python code as a hex string and embed it into the 'import binascii; eval(binascii.unhexlify(\"...\"))' string" do
+    it "must encode the given Python code as a hex string and embed it into the 'import binascii; exec(binascii.unhexlify(\"...\"))' string" do
       expect(subject.encode(python)).to eq(encoded)
     end
   end
